21c2bb0d7dcb30c9f19eab53e23ef8d3cfa888b6
RudyBecker <52896529+RudyBecker@users.noreply.github.com>
2020-11-02 22:19:13 -0800

    second commit

--- src/App.vue
+++ src/App.vue
@@ -1,133 +1,36 @@
 <template>
   <div id="main" :class="isDay ? 'day' : 'night'">
-    <div class="container my-5">
-      <h1 class="title text-center">Weather in</h1>
-      <p>{{ weather.cityName }}</p>
-      <p class="">{{ weather.country }}</p>
-      <span>{{ weather.temperature }}&deg;C</span>
-      <p class="my-4">{{ weather.description }}</p>
-      <p>
-        <!-- <img src="./assets/down.svg" alt="" /> -->
-        {{ weather.lowTemp }}&deg;C
-      </p>
-      <p>
-        <!-- <img src="./assets/up.svg" alt="" /> -->
-        {{ weather.highTemp }}&deg;C
-      </p>
-      <div class="col text-center">
-        <p>{{ weather.feelsLike }}&deg;C</p>
-        <span>Feels like</span>
-      </div>
-      <div class="col text-center">
-        <p>{{ weather.humidity }}%</p>
-        <span>humidity</span>
-      </div>
-      <!-- <form class="search-location" v-on:submit.prevent="getWeather">
-        <input
+    <div>
+      <h1 class="main title text-center">Weather in</h1>
+      <form v-on:submit.prevent="getWeather">
+        <b-form-input
           type="text"
-          class="form-control text-muted form-rounded p-4 shadow-sm"
-          placeholder="What City?"
+          class="text-muted my-5 shadow-sm w-25 mx-auto"
+          placeholder="Enter city name to view weather"
           v-model="citySearch"
           autocomplete="off"
         />
-      </form> -->
-      <!-- <p class="text-center my-3" v-if="cityFound">No city found</p> -->
-      <div
-        class="card rounded my-3 shadow-lg back-card overflow-hidden"
-        v-if="visible"
-      >
-        <!-- weather animation container -->
-        <!-- <div>
-          <div icon="sunny" v-if="clearSky">
-            <span class="sun"></span>
-          </div>
+      </form>
+      <p class="text-center my-3" v-if="cityFound">No city found</p>
 
-          <div icon="snowy" v-if="snowy">
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
-
-          <div icon="stormy" v-if="stormy">
-            <span class="cloud"></span>
-            <ul>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-              <li></li>
-            </ul>
-          </div>
-          <div icon="cloudy" v-if="cloudy">
-            <span class="cloud"></span>
-            <span class="cloud"></span>
-          </div>
-          <div icon="nightmoon" v-if="clearNight">
-            <span class="moon"></span>
-            <span class="meteor"></span>
-          </div>
-        </div> -->
-
-        <!-- Top of card starts here -->
-        <div class="card-top text-center" style="margin-bottom: 15rem">
-          <div class="city-name my-3">
+      <div class="flip-card mx-auto" style="background:red" v-if="visible">
+        <div class="flip-card-inner">
+          <div class="flip-card-front">
             <p>{{ weather.cityName }}</p>
-            <span>...</span>
-            <p class="">{{ weather.country }}</p>
+            <img
+              :src="image_url + weather.icon"
+              alt="Avatar"
+              style="width:300px;height:300px;"
+            />
           </div>
-        </div>
-        <!-- top of card ends here -->
-
-        <!--card middle body, card body used cos I want to just update the innerHTML -->
-        <div class="card-body">
-          <!-- card middle starts here -->
-          <div class="card-mid">
-            <div class="row">
-              <div class="col-12 text-center temp">
-                <span>{{ weather.temperature }}&deg;C</span>
-                <p class="my-4">{{ weather.description }}</p>
-              </div>
-            </div>
-            <div class="row">
-              <div class="col d-flex justify-content-between px-5 mx-5">
-                <p>
-                  <!-- <img src="./assets/down.svg" alt="" /> -->
-                  {{ weather.lowTemp }}&deg;C
-                </p>
-                <p>
-                  <!-- <img src="./assets/up.svg" alt="" /> -->
-                  {{ weather.highTemp }}&deg;C
-                </p>
-              </div>
-            </div>
+          <div class="flip-card-back">
+            <p>{{ weather.cityName }}</p>
+            <span>{{ weather.temperature }}&deg;F</span>
+            <p>{{ weather.lowTemp }}&deg;F</p>
+            <p>{{ weather.highTemp }}&deg;F</p>
+            <p>{{ weather.feelsLike }}&deg;F</p>
+            <p>{{ weather.humidity }}%</p>
           </div>
-          <!-- card middle ends here -->
-
-          <!-- card bottom starts here -->
-          <div class="card-bottom px-5 py-4 row">
-            <div class="col text-center">
-              <p>{{ weather.feelsLike }}&deg;C</p>
-              <span>Feels like</span>
-            </div>
-            <div class="col text-center">
-              <p>{{ weather.humidity }}%</p>
-              <span>humidity</span>
-            </div>
-          </div>
-
-          <!-- card bottom ends here -->
         </div>
       </div>
     </div>
@@ -147,19 +50,146 @@ export default {
     isDay: true,
     citySearch: "",
     weather: {
-      cityName: "Gwarinpa",
-      country: "NG",
-      temperature: 12,
-      description: "Clouds everywhere",
-      lowTemp: "19",
-      highTemp: "30",
-      feelsLike: "20",
-      humidity: "55",
+      cityName: "",
+      country: "",
+      temperature: "",
+      description: "",
+      lowTemp: "",
+      highTemp: "",
+      feelsLike: "",
+      humidity: "",
+      icon: "",
     },
+    image_url: "",
   }),
+  mounted() {
+    this.image_url = "http://openweathermap.org/img/wn/";
+  },
+  methods: {
+    //fetch and assign weather data
+    getWeather: async function() {
+      console.log(this.citySearch);
+      const key = "bb047a2b68262787a19ee0c198ae9661";
+      const baseURL = `http://api.openweathermap.org/data/2.5/weather?q=${this.citySearch}&appid=${key}&units=imperial`;
+
+      try {
+        const response = await fetch(baseURL);
+        const data = await response.json();
+        console.log(data);
+        this.citySearch = "";
+        this.weather.cityName = data.name;
+        this.weather.country = data.sys.country;
+        this.weather.temperature = Math.round(data.main.temp);
+        this.weather.description = data.weather[0].description;
+        this.weather.lowTemp = Math.round(data.main.temp_min);
+        this.weather.highTemp = Math.round(data.main.temp_max);
+        this.weather.feelsLike = Math.round(data.main.feels_like);
+        this.weather.humidity = Math.round(data.main.humidity);
+
+        this.weather.icon = data.weather[0].icon;
+        console.log(this.weather.icon);
+        // this.weather.iconLink = `http://openweathermap.org/img/wn/${this.weather.icon}@2x.png`;
+
+        const timeOfDay = data.weather[0].icon;
+
+        //check for time of day
+        if (timeOfDay.includes("n")) {
+          this.isDay = false;
+        } else {
+          this.isDay = true;
+        }
+
+        this.visible = true;
+      } catch (error) {
+        this.cityFound = true;
+        this.visible = false;
+      }
+    },
+  },
+  // computed: {
+  //   iconImage: {
+  //     get: function() {
+  //       return this.iconLink;
+  //     },
+  //     set: function() {
+  //       this.iconLink = `http://openweathermap.org/img/wn/${this.weather.icon}@2x.png`;
+  //     },
+  //   },
+  // },
 };
 </script>
 
 <style>
-/* @import "./assets/custom.css"; */
+* #main {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  margin: 0 auto;
+}
+.day {
+  background: url(./assets/day.png) no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+.night {
+  background: url(./assets/night.png) no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+}
+
+.title {
+  font-size: 50px;
+  font-weight: 500;
+}
+
+/* The flip card container - set the width and height to whatever you want. We have added the border property to demonstrate that the flip itself goes out of the box on hover (remove perspective if you don't want the 3D effect */
+.flip-card {
+  background-color: transparent;
+  width: 300px;
+  height: 200px;
+  border: 1px solid #f1f1f1;
+  perspective: 1000px; /* Remove this if you don't want the 3D effect */
+}
+
+/* This container is needed to position the front and back side */
+.flip-card-inner {
+  position: relative;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  transition: transform 0.8s;
+  transform-style: preserve-3d;
+}
+
+/* Do an horizontal flip when you move the mouse over the flip box container */
+.flip-card:hover .flip-card-inner {
+  transform: rotateY(180deg);
+}
+
+/* Position the front and back side */
+.flip-card-front,
+.flip-card-back {
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  -webkit-backface-visibility: hidden; /* Safari */
+  backface-visibility: hidden;
+}
+
+/* Style the front side (fallback if image is missing) */
+.flip-card-front {
+  background-color: #bbb;
+  color: black;
+}
+
+/* Style the back side */
+.flip-card-back {
+  background-color: dodgerblue;
+  color: white;
+  transform: rotateY(180deg);
+}
 </style>
